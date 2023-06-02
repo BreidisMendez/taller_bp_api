e75d078f6cecd8dd0b7196fe9a4381c39f5e566d
breidis <breidismendez3@gmail.com>
2023-06-02 02:21:47 -0500

    Commit dos

--- src/components/PersonajesMarvel.jsx
+++ src/components/PersonajesMarvel.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const PersonajesMarvel = () => {
+
+    const [Personajes, setPersonajes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div>PersonajesMarvel</div>
   )
