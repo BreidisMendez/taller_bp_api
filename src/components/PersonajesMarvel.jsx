f59245f1b338f49a94412a0e27f6c8e1f0b50478
breidis <breidismendez3@gmail.com>
2023-06-02 02:23:01 -0500

    Commit cuatro

--- src/components/PersonajesMarvel.jsx
+++ src/components/PersonajesMarvel.jsx
@@ -12,6 +12,18 @@ const PersonajesMarvel = () => {
         }
       }, [isLoading]);
 
+      const fetchPersonajes = async () => {
+        const apiKey = 'a4d59c5704cd70b188a6dc485879f63a';
+        const offset = currentPage * 10; // Multiplica por el límite para obtener el offset correcto
+        const response = await fetch(
+          `https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=${apiKey}&hash=e831972e0007719d4a7d0d8a4c71f556&offset=${offset}`
+        );
+        const data = await response.json();
+        setPersonajes(data.data.results);
+        setIsLoading(false);
+        console.log(data)
+      };
+
   return (
     <div>PersonajesMarvel</div>
   )
